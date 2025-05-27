5c3b4f8cc83f12f235e42eaeae4c68360c952db7
Your Name <you@example.com>
2025-05-27 15:49:03 +0100

    domain_name fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')).render(
 
         {/* Authentication */}
         <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{
-          redirect_uri: 'window.location.origin',
+          redirect_uri: 'https://skill-sync-2bln.onrender.com',
           audience:audience}}>
 
             {/* Chakra Provider */}
